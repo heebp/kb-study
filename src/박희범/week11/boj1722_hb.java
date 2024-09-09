ac6c0b9192aeca9285fab5b6c30d09ef8b38be8a
heebp <skdnfl4343@gmail.com>
2024-09-10 07:43:40 +0900

    add : 박희범/week11/boj1722

---
+++ src/박희범/week11/boj1722_hb.java
@@ -0,0 +1,19 @@
+package 박희범.week11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1722_hb {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        if (num == 1){
+
+        }
+
+    }
+}
